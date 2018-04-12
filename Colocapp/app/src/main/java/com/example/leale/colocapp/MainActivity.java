eb6869545c90242cde4853867f004dc9f4a1d9b4
Lea <lea.levy406@gmail.com>
2018-04-12 12:17:01 +0200

    boutons fonctionnels

--- Colocapp/app/src/main/java/com/example/leale/colocapp/MainActivity.java
+++ Colocapp/app/src/main/java/com/example/leale/colocapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.leale.colocapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void planning (View view) {
+        startActivity(new Intent(this, Activity2.class));
+    }
+    public void liste_course (View view) {
+
+        startActivity(new Intent(this, Activity3.class));
+    }
+    public void money (View view) {
+        startActivity(new Intent(this, Activity4.class));
+    }
+    public void scoreboard (View view) {
+        startActivity(new Intent(this, Activity5.class));
+    }
+    public void nous (View view) {
+        startActivity(new Intent(this, Activity6.class));
     }
 }
