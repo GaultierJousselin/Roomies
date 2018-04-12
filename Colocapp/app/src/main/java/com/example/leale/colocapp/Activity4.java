4c1ea6cbaa097d1b2e77f7bb79863571755312ff
Quentin MULLIEZ <33511073+Druwww@users.noreply.github.com>
2018-04-12 16:54:45 +0200

    On est passé sur activity_4 ^^
    Co-Authored-By: Gaultier Jousselin <gaultier.jousselin@orange.fr>

--- Colocapp/app/src/main/java/com/example/leale/colocapp/Activity4.java
+++ Colocapp/app/src/main/java/com/example/leale/colocapp/Activity4.java
@@ -1,13 +1,53 @@
 package com.example.leale.colocapp;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity4 extends AppCompatActivity {
+
+
+
+    public class DataPerson {
+
+        string m_name;
+        int m_ID_name_text;
+        int m_ID_button_minus;
+        int m_ID_button_plus;
+        int m_ID_button_delete;
+        int m_ID_button_points;
+        int m_points;
+        bool m_inuse;
+
+        public DataPerson();
+        public DataPerson(String pname, int ptext, int pminus, int pplus, int pdelete, int pbpoints)
+        {
+            m_name = pname;
+            m_ID_name_text = ptext;
+            m_ID_button_points = pbpoints;
+            m_ID_button_minus = pminus;
+            m_ID_button_plus = pplus;
+            m_ID_button_delete = pdelete;
+            m_points = 0
+            m_inuse = false;
+        }
+    }
+
+    vector <DataPerson> v_Person;
+
+    public void initialize(){
+        v_Person.add(new DataPerson("Quentin", R.id.textView1_name, R.id.button1_minus, R.id.button1_plus, R.id.button1_delete, R.id.textView1_points));
+    }
+
+    initialize();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +64,21 @@ public class Activity4 extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+        final EditText entry = (EditText) findViewById(R.id.editText);
+
+        final TextView result = (TextView) findViewById(R.id.textView4);
+
+
+        Button b = (Button) findViewById(R.id.button8);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText(entry.getText());
+            }
+        });
+
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 }
--- Colocapp/app/src/main/java/com/example/leale/colocapp/Activity5.java
+++ Colocapp/app/src/main/java/com/example/leale/colocapp/Activity5.java
@@ -5,34 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import android.view.View;
 
 public class Activity5 extends AppCompatActivity {
 
-    Button addPeople = null;
-
-    EditText nameEnter = null;
-
-    TextView textResult = null;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        ///Code default
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_5);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -46,30 +25,8 @@ public class Activity5 extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        ///Fin code defaut
-
-
-
-        // On récupère le bouton par son identifiant
-        addPeople = (Button) findViewById(R.id.ButtonValidEntryName);
-        // On récupère le textedit par son identifiant
-        nameEnter = (EditText)findViewById(R.id.EntryNameString);
-
-        //test si le bouton marche
-        textResult = (TextView)findViewById(R.id.textTestButton);
-
-
-        // Puis on lui indique que cette classe sera son listener pour l'évènement Touch
-        addPeople.setOnClickListener(envoyerListener);
-    };
-
-    // Uniquement pour le bouton "envoyer"
-    private OnClickListener envoyerListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            textResult.setText(nameEnter.getText().toString());
-        }
-    };
+    }
 
 }
